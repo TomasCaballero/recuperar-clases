3615d9f6b0033ca3668c712bbf3c4d0b67708aa2
Tomas Caballero <caballerotomas97@gmail.com>
2023-11-30 15:02:54 -0300

    Probando con ayuda de Bard

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,52 +1,110 @@
-import {  useEffect, useState } from 'react';
+// import {  useEffect, useState } from 'react';
+// import { clases } from '../Assets/Videos';
+// import ReactPlayer from 'react-player';
+// import './App.css'
+
+// function App() {
+//   const [clasesArray, setClasesArray] = useState([]);
+
+//   useEffect(()=>{
+//     const cargarVideos = () => {
+//       setClasesArray(clases);
+//     }
+
+//     setTimeout(()=>{
+//       cargarVideos();
+//     },1000)
+
+//   },[clasesArray])
+
+//   return (
+//     <div className='layout'>
+
+//       <h1>Clases Diplomatura</h1>
+//       <div className="clasesContainer">
+//         {
+//           clasesArray === clases ? (
+//             clasesArray.map((clase) => {
+//               return (
+//                 <ReactPlayer key={clase.url} url={clase.url} controls className='video' />
+//               )
+//             })
+//           ) : (
+//             <h3 className='mensajeCargando'>Cargando Videos...</h3>
+//           )
+//         }
+//       </div>
+
+//       <div className="clasesContainer">
+//         {
+//           clasesArray.map((claseUrl)=> {
+//             return(
+//               <a href={claseUrl.url} target="_blank" rel="noopener noreferrer" key={claseUrl.url}>{claseUrl.url}</a>
+//             )
+//           })
+//         }
+//       </div>
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+import { useEffect, useState } from 'react';
 import { clases } from '../Assets/Videos';
 import ReactPlayer from 'react-player';
-import './App.css'
+import './App.css';
 
 function App() {
   const [clasesArray, setClasesArray] = useState([]);
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     const cargarVideos = () => {
       setClasesArray(clases);
-    }
+    };
 
-    setTimeout(()=>{
+    setTimeout(() => {
       cargarVideos();
-    },1000)
-
-  },[clasesArray])
+    }, 1000);
+  }, [clasesArray]);
 
   return (
-    <div className='layout'>
-
+    <div className="layout">
       <h1>Clases Diplomatura</h1>
+
       <div className="clasesContainer">
-        {
-          clasesArray === clases ? (
-            clasesArray.map((clase) => {
-              return (
-                <ReactPlayer key={clase.url} url={clase.url} controls className='video'/>
-              )
-            })
-          ) : (
-            <h3 className='mensajeCargando'>Cargando Videos...</h3>
-          )
-        }
+        {clasesArray === clases ? (
+          clasesArray.map((clase) => (
+            <ReactPlayer
+              key={clase.url}
+              url={clase.url}
+              controls
+              className="video"
+              headers={(range) => ({
+                'Content-Range': range,
+              })}
+            />
+          ))
+        ) : (
+          <h3 className="mensajeCargando">Cargando Videos...</h3>
+        )}
       </div>
 
       <div className="clasesContainer">
-        {
-          clasesArray.map((claseUrl)=> {
-            return(
-              <a href={claseUrl.url} target="_blank" rel="noopener noreferrer" key={claseUrl.url}>{claseUrl.url}</a>
-            )
-          })
-        }
+        {clasesArray.map((claseUrl) => (
+          <a
+            href={claseUrl.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            key={claseUrl.url}
+          >
+            {claseUrl.url}
+          </a>
+        ))}
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
