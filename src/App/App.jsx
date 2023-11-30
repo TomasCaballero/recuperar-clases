6155942d6abbb6d13069d03bf5e8f2fa99ca1632
Tomas Caballero <caballerotomas97@gmail.com>
2023-11-30 15:33:58 -0300

    Probando con un solo video con rutas relativas y asincrona

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,61 @@
-import {  useEffect, useState } from 'react';
+// import {  useEffect, useState } from 'react';
+// import ReactPlayer from 'react-player';
+// import './App.css'
+// import { clases } from '../../public/Assets/Videos';
+
+// function App() {
+//   const [clasesArray, setClasesArray] = useState([]);
+
+//   const ruta = "https:zoom-fepp.s3.amazonaws.com/98808465138/98808465138"
+
+
+//   useEffect(()=>{
+//     const cargarVideos = () => {
+//       setClasesArray(clases);
+//     }
+
+//     setTimeout(()=>{
+//       cargarVideos();
+//     },1000)
+
+//   },[clasesArray])
+
+//   return (
+//     <div className='layout'>
+
+//       <h1>Clases Diplomatura</h1>
+//       <div className="clasesContainer">
+//         {
+//           clasesArray === clases ? (
+//             clasesArray.map((clase) => {
+//               return (
+//                 <ReactPlayer key={clase.url} url={ruta+clase.url} controls className='video' />
+//               )
+//             })
+//           ) : (
+//             <h3 className='mensajeCargando'>Cargando Videos...</h3>
+//           )
+//         }
+//       </div>
+
+//       <div className="clasesContainer">
+//         {
+//           clasesArray.map((claseUrl)=> {
+//             return(
+//               <a href={ruta+claseUrl.url} target="_blank" rel="noopener noreferrer" key={ruta+claseUrl.url}>{ruta+claseUrl.url}</a>
+//             )
+//           })
+//         }
+//       </div>
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
 import './App.css'
 import { clases } from '../../public/Assets/Videos';
@@ -9,47 +66,46 @@ function App() {
   const ruta = "https:zoom-fepp.s3.amazonaws.com/98808465138/98808465138"
 
 
-  useEffect(()=>{
-    const cargarVideos = () => {
-      setClasesArray(clases);
-    }
+  useEffect(() => {
+    const cargarVideos = async () => {
+      // Crear un array de promesas para la carga de videos
+      const promises = clases.map(async (clase) => {
+        // Aquí podrías realizar lógica adicional si es necesario antes de cargar el video
+        return await new Promise((resolve) => {
+          // Resuelve la promesa cuando el video está cargado y listo para reproducirse
+          resolve({
+            ...clase,
+            loaded: true,
+          });
+        });
+      });
 
-    setTimeout(()=>{
-      cargarVideos();
-    },1000)
+      // Espera a que todas las promesas se resuelvan
+      const loadedVideos = await Promise.all(promises);
+      setClasesArray(loadedVideos);
+    };
 
-  },[clasesArray])
+    cargarVideos();
+  }, []);
 
   return (
     <div className='layout'>
-
       <h1>Clases Diplomatura</h1>
       <div className="clasesContainer">
-        {
-          clasesArray === clases ? (
-            clasesArray.map((clase) => {
-              return (
-                <ReactPlayer key={clase.url} url={ruta+clase.url} controls className='video' />
-              )
-            })
-          ) : (
-            <h3 className='mensajeCargando'>Cargando Videos...</h3>
-          )
-        }
+        {clasesArray.map((clase) => (
+          <ReactPlayer key={clase.url} url={ruta + clase.url} controls className='video' />
+        ))}
       </div>
 
       <div className="clasesContainer">
-        {
-          clasesArray.map((claseUrl)=> {
-            return(
-              <a href={ruta+claseUrl.url} target="_blank" rel="noopener noreferrer" key={ruta+claseUrl.url}>{ruta+claseUrl.url}</a>
-            )
-          })
-        }
+        {clasesArray.map((claseUrl) => (
+          <a href={ruta + claseUrl.url} target="_blank" rel="noopener noreferrer" key={ruta + claseUrl.url}>
+            {ruta + claseUrl.url}
+          </a>
+        ))}
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
